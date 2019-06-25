f9fc3a564706edf91b7557ae316d9d4a810c1829
youssef-rsy <youssef.rsy@gmail.com>
2019-06-25 17:34:32 +0100

    data mpping between component listAccount - DeleteAcount

--- src/component/DeleteAccount.js
+++ src/component/DeleteAccount.js
@@ -1,23 +1,24 @@
-import React from "react";
+import React ,{Component} from "react";
 
-export default function DeletePromp(){
-
-    return (
-          <div className="modal fade" id="deletePromp" tabIndex={-1} role="dialog" aria-labelledby="deletePromp" aria-hidden="true">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Are you sure that you wanna delete this account ?</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-              </button>
+//export default class DeletePromp extends Component {
+    const DeletePromp =({deleteAccount , accountId})=>{
+        return (
+            <div className="modal fade" id="deletePromp" tabIndex={-1} role="dialog" aria-labelledby="deletePromp" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+            <div className="modal-content">
+                <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Are you sure that you wanna delete this account ?</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+                </div>
+                <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" className="btn btn-danger" onClick={()=>{deleteAccount(accountId)}} >Delete</button>
+                </div>
             </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-              <button type="button" className="btn btn-danger">Delete</button>
             </div>
-          </div>
         </div>
-      </div>
-    )
+        )
 }
+export default  DeletePromp;
--- src/component/ListAccount.js
+++ src/component/ListAccount.js
@@ -6,10 +6,20 @@ import DeletePromp from './DeleteAccount';
 import CreditPromp from './CreditAccount';
 import DebitPromp from './DebitAccount';
 class App extends Component {
+  
   state={
+    acountId:null,
     accounts:[
       {accountId:1 , accountOwner: "youssef rossamy", balance: "21130.22", creationDate: "2019-06-29"},
       {accountId:2 , accountOwner: " youssef jamoure", balance: "51130.99", creationDate: "2018-02-28"}]
+  }
+  selectedAccount=(id)=>{
+    this.setState({
+      acountId:id
+    })
+  }
+  deleteAccount=(id)=>{
+    console.log(id);
   }
   render(){
     const accounts = this.state.accounts.map(account=>{
@@ -19,14 +29,26 @@ class App extends Component {
             <td>{account.balance}</td>
             <td>{account.creationDate}</td>
             <td className="w-50">
-                <a className=""> <img data-toggle="modal" data-target="#creditPromp" src={Plus} width="3%" /></a>
-                <a className=""> <img data-toggle="modal" data-target="#debitPromp" src={Remove} width="3%" /></a>
-                <a className=""> <img  data-toggle="modal" data-target="#deletePromp" src={Delete} width="3%" /></a>
+                <a className=""> 
+                  <img data-toggle="modal" data-target="#creditPromp" src={Plus} width="3%" />
+                  
+                  </a>
+                <a className="">
+                   <img data-toggle="modal" data-target="#debitPromp" src={Remove} width="3%" />
+                  
+                   </a>
+                <a className=""> 
+                  <img  data-toggle="modal" data-target="#deletePromp" src={Delete} width="3%" onClick={()=>{this.selectedAccount(account.accountId)}} />
+                  
+                </a>
             </td>
           </tr>
     )});
     return (
       <div className="container row col-12 p-5">
+      <CreditPromp/>
+      <DebitPromp/>
+      <DeletePromp deleteAccount={this.deleteAccount} accountId={this.state.acountId} />
       <table className="table col-12">
           <thead>
             <tr>
@@ -40,9 +62,7 @@ class App extends Component {
               {accounts}            
           </tbody>
         </table>
-        <DeletePromp/>
-        <CreditPromp/>
-        <DebitPromp/>
+        
       </div>
     );
   }
