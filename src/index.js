967565ffbcd8667b5a5eeac5d56d1b7e3ec02fc5
Youssef-rsy <youssef.rsy@gmail.com>
2019-09-25 20:20:21 +0100

    axios functionality  - axios instance

--- src/index.js
+++ src/index.js
@@ -6,6 +6,33 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+import axios from "axios";
+
+//setting base Url globaly
+//axios.defaults.baseURL = 'https://accountservicereact.herokuapp.com/account';
+
+//how to set a common header on request
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+
+//how to set a common header on request on specefic method
+axios.defaults.headers.post['Content-Type'] =  'application/json';
+
+  axios.interceptors.request.use(requestConfg=>{
+    console.log("request : "+requestConfg);
+    //Edit request config
+    return requestConfg;
+},error=>{
+    console.log("axios request intercetor error :"+error);
+    return Promise.reject();
+});
+
+axios.interceptors.response.use(responsetConfg=>{
+    console.log("response : "+responsetConfg);
+    return responsetConfg;
+},error=>{
+    console.log("axios response intercetor error :"+error);
+    return Promise.reject();
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/service/AccountService.js
+++ src/service/AccountService.js
@@ -1,5 +1,33 @@
-import axios from "axios"
+import axios from "axios";
 
-export default axios.create({
+
+const instance = axios.create({
     baseURL: 'https://accountservicereact.herokuapp.com/account'
   });
+
+//how to set a common header on request
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN from instance';
+
+//how to set a common header on request on specefic method
+instance.defaults.headers.post['Content-Type'] =  'application/json';
+
+
+instance.interceptors.request.use(requestConfg=>{
+    console.log("request : ",requestConfg);
+    //requestConfg.timeout=5000000;
+    //Edit request config
+    return requestConfg;
+},error=>{
+    console.log("axios request intercetor error :"+error);
+    return Promise.reject();
+});
+
+instance.interceptors.response.use(responsetConfg=>{
+    console.log("response : ",responsetConfg);
+    return responsetConfg;
+},error=>{
+    console.log("axios response intercetor error :"+error);
+    return Promise.reject();
+});
+
+export default instance;
